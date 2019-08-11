71950346a38b8e1108ad26a458bfef1b05a6a7c8
gayathri.m <53171194+Gayathri200@users.noreply.github.com>
2019-08-11 18:20:24 +0530

    Create exp1.py

---
+++ exp1.py
@@ -0,0 +1,16 @@
+x,y=map(str,input().split())
+a=len(x)
+b=len(y)
+r=0
+if(b>=a):
+  for i in range(0,a):
+    if(x[i]==y[i]):
+      r=r+1
+  b=b-r
+  print(b)
+elif(a>b):
+  for i in range(0,b):
+    if(x[i]==y[i]):
+      r=r+1
+  a=a-r
+  print(a) 
